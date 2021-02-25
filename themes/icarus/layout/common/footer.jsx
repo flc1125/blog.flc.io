8bd6fbdb9f3834ea397cb4453c34b8fdceae98cb
flc1125 <four_leaf_clover@foxmail.com>
2021-02-25 13:09:57 +0800

    add article & config

--- themes/icarus/layout/common/footer.jsx
+++ themes/icarus/layout/common/footer.jsx
@@ -34,7 +34,7 @@ class Footer extends Component {
                             {footerLogo}
                         </a>
                         <p class="is-size-7">
-                            <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
+                            <span dangerouslySetInnerHTML={{ __html: `&copy; 2017-${siteYear} ${author || siteTitle}` }}></span>
                             &nbsp;&nbsp;Powered by <a href="https://hexo.io/" target="_blank" rel="noopener">Hexo</a>&nbsp;&&nbsp;
                             <a href="https://github.com/ppoffice/hexo-theme-icarus" target="_blank" rel="noopener">Icarus</a>
                             {showVisitorCounter ? <br /> : null}
